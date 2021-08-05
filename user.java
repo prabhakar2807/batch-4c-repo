f250ea0b77ef5f9cd6cb4d4bb5112bcf27de72e7
Prabhakar Sharma <prabhsha@amdocs.com>
2021-08-05 15:32:16 +0530

    PRIME METHOD ADDED

--- user.java
+++ user.java
@@ -3,5 +3,19 @@ class User
 public static void main(String args[])
 {
 System.out.println("Git repository");
+System.out.println("prime method added " + isprime());
+public static int isPrime(int number)
+{
+	for(int i=2;i<num/2;i++)
+	{
+		if(num%2==0)
+		{
+			return false;
+			break;
+		}
+		return true;
+		
+	}
+}
 }
 }
